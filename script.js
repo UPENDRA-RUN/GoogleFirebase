7b47a156a7dadaa5bfb0195d7c1067d7e63f6046
UPENDRA-RUN <upendrabhadouriya213@gmail.com>
2025-08-20 21:35:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
 
-// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBgwK2PJi2Bul38KGeG4Kt9mvkBdtSxVX4",
+  apiKey: "YOUR_API_KEY",
   authDomain: "myfirebaseproject-7ca83.firebaseapp.com",
   projectId: "myfirebaseproject-7ca83",
   storageBucket: "myfirebaseproject-7ca83.appspot.com",
@@ -11,25 +10,21 @@ const firebaseConfig = {
   appId: "1:655832718604:web:6a1925fc5d10d314a83d29"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Setup Auth and Google provider
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
-auth.languageCode = 'en'
 
 const googleLogin = document.getElementById("google-login");
+
 googleLogin.addEventListener("click", function () {
   signInWithPopup(auth, provider)
     .then((result) => {
       const user = result.user;
-      console.log("User logged in:", user.displayName);
       alert("Welcome " + user.displayName);
-      window.addEventListener("error", (e) => {
-        console.error("Error occurred:", e.message);
-      });
-
+      console.log("User logged in:", user);
+    })
+    .catch((error) => {
+      console.error("Login failed:", error.message);
+      alert("Error: " + error.message);
     });
 });
-
